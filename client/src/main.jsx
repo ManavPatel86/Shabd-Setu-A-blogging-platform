ef1a172b2c5b2b76187c0452152b71bb97e690d2
Tushal-Mendpara <tushalmendpara@gmail.com>
2025-09-21 16:37:01 +0530

    Completed Redux setup with storing the current state of user

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux'
-import {store} from './store'
+import {store} from './store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './redux/user/user.slice'
 
-export default configureStore({
-  reducer: { user: userReducer },
+export const store = configureStore({
+    reducer: { user: userReducer },
 })
+
