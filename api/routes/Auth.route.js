6472bd6e26fad901c33ebd7c27fe212ee6fe9f54
VedDonda <veddonda4835@gmail.com>
2025-09-21 18:38:28 +0530

    Add Profile Dropdown

--- api/routes/Auth.route.js
+++ api/routes/Auth.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import {  Login,  Register } from '../controllers/Auth.controller.js'
+import {  Login,  Register ,Logout} from '../controllers/Auth.controller.js'
 
 const AuthRoute = express.Router()
 
 AuthRoute.post('/register', Register)
 AuthRoute.post('/login', Login)
-
+AuthRoute.get('/logout', Logout)
 
 export default AuthRoute
