7efecd95e982c6576c6dd2f0f4dd456e7e87ab8d
Tushal-Mendpara <tushalmendpara@gmail.com>
2025-09-21 16:56:53 +0530

    Resolved issue of automatic log out when refreshing page

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './redux/user/user.slice'
+import sessionStorage from 'redux-persist/es/storage/session'
+import persistReducer from 'redux-persist/es/persistReducer'
+import { persistStore } from 'redux-persist'
 
-export const store = configureStore({
-    reducer: { user: userReducer },
+const rootReducer = combineReducers({
+    user: userReducer,
 })
 
+const persistConfig = {
+    key: 'root',
+    storage: sessionStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+})
+
+export const persistor = persistStore(store)
