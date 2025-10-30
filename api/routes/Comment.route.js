6ba8f521fa9448abdbc6fdd7d6f59aae06c32a35
Dhruvika-rathod168 <202301168@dau.ac.in>
2025-10-30 23:46:16 +0530

    Create Comment.route.js

--- api/routes/Comment.route.js
+++ api/routes/Comment.route.js
@@ -2,7 +2,6 @@ import express from 'express'
 import { addcomment } from '../controllers/Comment.controller.js'
 
 const CommentRouote = express.Router()
-
 CommentRouote.post('/add', authenticate, addcomment)
 
 
