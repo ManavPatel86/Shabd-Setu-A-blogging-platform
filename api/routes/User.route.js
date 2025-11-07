1ea6217105e2aad0a8d1eb9481cffe4a5ac409cf
Shubham Kankotiya <202301155@dau.ac.in>
2025-11-07 15:08:18 +0530

    Change Whole Profile Page,Add Heatmap,Add Public Profile

--- api/routes/User.route.js
+++ api/routes/User.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, getAllUser, getUser, updateUser, updateUserBlacklistStatus } from '../controllers/User.controller.js'
+import { deleteUser, getAllUser, getUser, getUserContributionActivity, getUserProfileOverview, updateUser, updateUserBlacklistStatus } from '../controllers/User.controller.js'
 import upload from '../config/multer.js'
 import { authenticate } from '../middleware/authenticate.js'
 
@@ -8,6 +8,8 @@ const UserRoute = express.Router()
 UserRoute.use(authenticate)
 
 UserRoute.get('/get-user/:userid', getUser)
+UserRoute.get('/profile-overview/:userid', getUserProfileOverview)
+UserRoute.get('/contributions/:userid', getUserContributionActivity)
 UserRoute.put('/update-user/:userid', upload.single('file'), updateUser)
 UserRoute.get('/get-all-user', getAllUser)
 UserRoute.delete('/delete/:id', deleteUser)
